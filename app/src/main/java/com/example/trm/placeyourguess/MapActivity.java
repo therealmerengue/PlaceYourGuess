21e36fcc9e47d9a8e2202084b37c816cc4dd81bc
therealmerengue <therealmerengue@gmail.com>
2017-03-22 23:04:45 +0100

    Basic singleplayer, added ScoreFragment.

--- app/src/main/java/com/example/trm/placeyourguess/MapActivity.java
+++ app/src/main/java/com/example/trm/placeyourguess/MapActivity.java
@@ -1,5 +1,11 @@
 package com.example.trm.placeyourguess;
 
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,27 +14,69 @@ import android.widget.Button;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements ScoreFragment.OnFragmentInteractionListener {
 
-    SupportMapFragment mFragMap;
-    GoogleMap map;
-    Marker mGuessedLocationMarker;
-    Button mBtnConfirm;
+    private SupportMapFragment mFragMap;
+    private GoogleMap mMap;
+    private Marker mGuessedLocationMarker;
+    private LatLng mPassedLocationCoords;
+
+    private Button mBtnConfirm;
+
+    final static String RESULT_KEY_DISTANCE = "DISTANCE";
+
+    private float mGuessOffset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        ScoreFragment.newInstance();
+        hideAndShowFragment(true);
+
+        Intent intent = getIntent();
+        double[] passedCoords = intent.getDoubleArrayExtra(StreetViewActivity.TAG_LOCATION_COORDINATES);
+        mPassedLocationCoords = new LatLng(passedCoords[0], passedCoords[1]);
+
         mBtnConfirm = (Button) findViewById(R.id.btn_confirm);
         mBtnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                LatLng markerCoords = mGuessedLocationMarker.getPosition();
 
+                Location markerLocation = new Location("");
+                markerLocation.setLatitude(markerCoords.latitude);
+                markerLocation.setLongitude(markerCoords.longitude);
+
+                Location passedLocation = new Location("");
+                passedLocation.setLatitude(mPassedLocationCoords.latitude);
+                passedLocation.setLongitude(mPassedLocationCoords.longitude);
+
+                mGuessOffset = markerLocation.distanceTo(passedLocation);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(mPassedLocationCoords)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                PolylineOptions line = new PolylineOptions()
+                        .add(mPassedLocationCoords, markerCoords)
+                        .width(5)
+                        .color(Color.BLUE);
+
+                mMap.addPolyline(line);
+
+                hideAndShowFragment(false);
+
+                android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+                ((ScoreFragment)fm.findFragmentById(R.id.frag_score)).setDistance(mGuessOffset);
+
+                mMap.setOnMapClickListener(null);
             }
         });
 
@@ -36,19 +84,42 @@ public class MapActivity extends AppCompatActivity {
         mFragMap.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                map = googleMap;
-                map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                mMap = googleMap;
+                mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(LatLng latLng) {
                         if (mGuessedLocationMarker != null) {
                             mGuessedLocationMarker.remove();
                         }
-                        mGuessedLocationMarker = map.addMarker(new MarkerOptions().position(latLng));
+                        mGuessedLocationMarker = mMap.addMarker(new MarkerOptions().position(latLng));
 
                         mBtnConfirm.setEnabled(true);
                     }
                 });
             }
         });
+    }
+
+    public void hideAndShowFragment(boolean hide) {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment pf = fm.findFragmentById(R.id.frag_score);
+        FragmentTransaction ft = fm.beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+        if (hide) {
+            ft.hide(pf).commit();
+        } else {
+            ft.show(pf).commit();
+        }
+    }
+
+    @Override
+    public void onScoreFragmentInteraction() {
+
+        Bundle resultData = new Bundle();
+        resultData.putFloat(RESULT_KEY_DISTANCE, mGuessOffset);
+        Intent intent = new Intent();
+        intent.putExtras(resultData);
+        setResult(RESULT_OK, intent);
+
+        finish();
     }
 }
--- app/src/main/java/com/example/trm/placeyourguess/StreetViewActivity.java
+++ app/src/main/java/com/example/trm/placeyourguess/StreetViewActivity.java
@@ -4,18 +4,27 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
 
+import static com.example.trm.placeyourguess.MapActivity.RESULT_KEY_DISTANCE;
+
 public class StreetViewActivity extends AppCompatActivity {
 
     private FloatingActionButton mBtnSwitchToMap;
 
     private StreetViewPanorama mStreetViewPanorama;
+    private LatLng mLocationCoords = new LatLng(37.765927, -122.449972);
+
+    static final String TAG_LOCATION_COORDINATES = "TAG_LOCATION_COORDINATES";
+
+    static final int REQ_MAP_ACTIVITY = 100;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -27,26 +36,42 @@ public class StreetViewActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(StreetViewActivity.this, MapActivity.class);
-                startActivity(intent);
+                intent.putExtra(TAG_LOCATION_COORDINATES, new double[] {mLocationCoords.latitude, mLocationCoords.longitude});
+                startActivityForResult(intent, REQ_MAP_ACTIVITY);
             }
         });
 
         SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
                 (SupportStreetViewPanoramaFragment) getSupportFragmentManager().findFragmentById(R.id.frag_streetview);
 
-        streetViewPanoramaFragment.getStreetViewPanoramaAsync(
-                new OnStreetViewPanoramaReadyCallback() {
-                    @Override
-                    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
-                        mStreetViewPanorama = panorama;
-                        mStreetViewPanorama.setStreetNamesEnabled(Settings.isStreetNamesEnabled());
-                        mStreetViewPanorama.setUserNavigationEnabled(true);
-                        mStreetViewPanorama.setZoomGesturesEnabled(true);
-                        mStreetViewPanorama.setPanningGesturesEnabled(true);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(new OnStreetViewPanoramaReadyCallback() {
+                @Override
+                public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+                    mStreetViewPanorama = panorama;
+                    mStreetViewPanorama.setStreetNamesEnabled(Settings.isStreetNamesEnabled());
+                    mStreetViewPanorama.setUserNavigationEnabled(true);
+                    mStreetViewPanorama.setZoomGesturesEnabled(true);
+                    mStreetViewPanorama.setPanningGesturesEnabled(true);
 
-                        mStreetViewPanorama.setPosition(new LatLng(37.765927, -122.449972));
-                    }
-                });
-
+                    mStreetViewPanorama.setPosition(mLocationCoords);
+                }
+            });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_MAP_ACTIVITY:
+                if (resultCode == RESULT_OK) {
+                    Bundle res = data.getExtras();
+                    float distance = res.getFloat(RESULT_KEY_DISTANCE); //TODO: calculate points based on that
+
+                    mStreetViewPanorama.setPosition(new LatLng(40.758896, -73.985130));
+                }
+                break;
+        }
+    }
+
 }
